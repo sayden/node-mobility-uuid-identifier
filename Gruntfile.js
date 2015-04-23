bdbd210a3fb632c7053ef032c2413127d5004ec7
Mario Castro <mcastro@atsistemas.com>
2015-04-23 18:25:28 +0200

    Progress

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,47 +2,85 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    nodeunit: {
-      files: ['test/**/*_test.js'],
-    },
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib: {
-        src: ['lib/**/*.js']
-      },
-      test: {
-        src: ['test/**/*.js']
-      },
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib: {
-        files: '<%= jshint.lib.src %>',
-        tasks: ['jshint:lib', 'nodeunit']
-      },
-      test: {
-        files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'nodeunit']
-      },
-    },
-  });
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+        jsdoc: {
+            dist : {
+                src: ['*.js', 'test/*.js'],
+                options: {
+                    destination: 'doc',
+                    template:"node_modules/grunt-jsdoc/node_modules/ink-docstrap/template",
+                    configure : "node_modules/grunt-jsdoc/node_modules/ink-docstrap/template/jsdoc.conf.json"
+                }
+            }
+        },
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit']);
+        codeclimate: {
+            options: {
+                file: "coverage/lcov.info",
+                token: ""
+            }
+        },
 
+        jshint: {
+            all: 'index.js',
+            options:{
+                jshintrc: '.jshintrc'
+            }
+        },
+
+        clean: {
+            coverage: {
+                src: ['coverage']
+            }
+        },
+        mochaTest: {
+            quick:{
+                options: {
+                    reporter: 'spec'
+                },
+                src: ['test/*.js']
+            },
+            cover:{
+                options:{
+                    reporter: 'spec'
+                },
+                src: ['coverage/instrument/*.js']
+            }
+        },
+
+        mocha_istanbul: {
+            coverage: {
+                src: 'test', // a folder works nicely
+                options: {
+                    mask: '*.js'
+                }
+            }
+        },
+        publish: {
+            main: {
+                src: '*'
+            }
+        }
+    });
+
+    //Load tasks
+    require('load-grunt-tasks')(grunt);
+
+    //Test task
+    grunt.registerTask('test', ['mochaTest']);
+
+    //Documentation
+    grunt.registerTask('doc', ['jsdoc']);
+
+    //Coverage
+    grunt.registerTask('coverage', ['jshint', 'clean:coverage', 'mocha_istanbul:coverage']);
+
+    //Publish
+    grunt.registerTask('publish', ['clean:coverage', 'jshint', 'mocha_istanbul:coverage']);
+
+    // Default task(s).
+    grunt.registerTask('default', ['mochaTest']);
 };
--- test/mobility-uuid-identifier_test.js
+++ test/mobility-uuid-identifier_test.js
@@ -2,35 +2,95 @@
 
 var mobility_uuid_identifier = require('../lib/mobility-uuid-identifier.js');
 
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
+/* Testing libraries */
+var chai = require('chai');
+var mocha = require('mocha');
+var should = chai.should();
+var Q = require('q');
 
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
+// iOS project
+var iOsFileToSearch = '.xcodeproj';
+var iOsProjectPath = '/Users/mariocastro/testProject';
 
-exports['awesome'] = {
-  setUp: function(done) {
-    // setup here
-    done();
-  },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(mobility_uuid_identifier.awesome(), 'awesome', 'should be awesome.');
-    test.done();
-  },
+// Android project
+var androidProjectPath = '/Users/mariocastro/app-demo-android';
+
+describe('Node UID project extractor', function(){
+
+    it('should return the path of a filename within an specified path', function(done){
+        mobility_uuid_identifier.getFilePath(iOsFileToSearch, iOsProjectPath).then(function(filePath){
+            filePath.indexOf(iOsFileToSearch).should.not.be.equals(-1);
+            done();
+        }).catch(done);
+    });
+
+    it('should return a true for an iOS project', function(done){
+        mobility_uuid_identifier.isIosProject(iOsProjectPath).then(function(isIOS){
+            isIOS.should.not.be.null;
+            done();
+        }).catch(done);
+    });
+
+    it('should return a true for an Android project', function(done){
+        mobility_uuid_identifier.isAndroidProject(androidProjectPath).then(function(isAndroid){
+            isAndroid.should.not.be.null;
+            done();
+        }).catch(done);
+    });
+
+    it('should return the android package name', function(done){
+        mobility_uuid_identifier.getAndroidPackageName(androidProjectPath).then(function(packageName){
+            packageName.should.contains('com.');
+            done();
+        }).catch(done);
+    });
+
+    it('should return the path to the info.plist file', function(done){
+        mobility_uuid_identifier.getInfoPlistRelative(iOsProjectPath).then(function(res){
+            res.indexOf('.plist').should.not.be.equals(-1);
+            done();
+        }).catch(done);
+    });
+
+    it.only('should return the ios bundleId', function(done){
+        mobility_uuid_identifier.getBundleidiOS('/tmp/temp').then(function(bundleId){
+            bundleId.should.be.a.string;
+//            bundleId.should.contains('atsistemas.');
+            console.log(bundleId);
+            done();
+        }).catch(done);
+    });
+
+    it('should return the bundleId of an iOs project', function(done){
+        //clone the repo
+        execPromise('git clone https://github.com/owncloud/ios.git /tmp/temp', {cwd:'/tmp'}).then(function(res){
+            return mobility_uuid_identifier.getBundleidiOS('/tmp/temp');
+        }).then(function(bundleId){
+            bundleId.should.be.a.string;
+            console.log(bundleId);
+            done();
+        }).catch(done);
+    })
+
+});
+
+/**
+ * Main function to use the command line tools to execute git commands
+ * @param command   Command to execute. Do not include 'git ' prefix
+ * @param options   Options available in exec command https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
+ * @returns {promise|*|Q.promise}
+ */
+var execPromise = function (command, options) {
+    var exec = require('child_process').exec;
+    var defer = Q.defer();
+
+    exec(command, options, function (err, stdout, stderr) {
+        if (err) {
+            defer.reject({err: err, stderr: stderr});
+        } else {
+            defer.resolve({res:stdout, out:stderr});
+        }
+    });
+
+    return defer.promise;
 };
